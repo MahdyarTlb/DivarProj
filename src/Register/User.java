0c5508307b8afcb6d95e6a8e021a40574433e790
MahdyarTlb <talebimahdyar8@gmail.com>
2025-03-15 14:37:42 +0330

    commiting

---
+++ src/Register/User.java
@@ -0,0 +1,31 @@
+package Register;
+
+public class User {
+    String UserName;
+    String Password;
+    int Score;
+
+    //Constructor
+    public User(String UserName, String Password){
+        this.UserName = UserName;
+        this.Password = Password;
+        this.Score = 0;
+    }
+
+    //Getters & Setters
+    public String getUserName(){
+        return UserName;
+    }
+
+    public String getPassword(){
+        return Password;
+    }
+
+    public int getScore(){
+        return Score;
+    }
+
+    public void setScore(int Score){
+        this.Score = Score;
+    }
+}
