1d6663cfe8889a6b6566ca54055e433760ffec1e
MahdyarTlb <talebimahdyar8@gmail.com>
2025-03-15 15:47:47 +0330

    commiting

---
+++ src/Register/AuthMenu.java
@@ -0,0 +1,51 @@
+package Register;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AuthMenu {
+    public static void main(String[] args) {
+        List<User> users = UserManager.LoadUsers();
+        Scanner scanner =  new Scanner(System.in);
+
+        while(true){
+            System.out.println("1. login \n2. register \n3. exit");
+            System.out.print("Enter your choice : ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); //clear buffer
+            switch(choice){
+                case 1:
+                    System.out.println("Enter your username : ");
+                    String userName = scanner.nextLine();
+                    System.out.println("Enter your password : ");
+                    String password = scanner.nextLine();
+                    for(User user : users){
+                        if(user.getUserName().equals(userName) && user.getPassword().equals(password)){
+                            System.out.println("Hi " + userName + " !");
+                            UserManager.saveUsers(users);
+                            return;
+                        }
+                        System.out.println("Your userName or password is incorrect! try again");
+                    }
+                    continue;
+                case 2:
+                    System.out.print("Enter your UserName : ");
+                    String UserName = scanner.nextLine();
+                    System.out.print("Enter your Password : ");
+                    String Password = scanner.nextLine();
+                    User user = new User(UserName, Password); //make new user
+                    users.add(user);
+                    UserManager.saveUsers(users);
+                    System.out.println("--------------------\nUser has been saved successfully\n--------------------");
+                    UserManager.saveUsers(users);
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    UserManager.saveUsers(users);
+                    break;
+
+            }
+        }
+    }
+}
--- src/Register/UserManager.java
+++ src/Register/UserManager.java
@@ -1,4 +1,39 @@
 package Register;
 
+import java.io.*;
+import java.io.FileWriter;
+import java.util.*;
 public class UserManager {
+    private static final String FILE_PATH =  "Users.txt";
+
+    //save users data
+    public static void saveUsers(List<User> users){
+        try(FileWriter Writer = new FileWriter(FILE_PATH,true)){
+            for(User user : users){
+                Writer.write(user.getUserName() + "," + user.getPassword() + "," + user.getScore());
+            }
+        } catch(IOException e){e.toString();}
+    }
+
+    //read user data
+    public static List<User> LoadUsers(){
+        List<User> users = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))){
+            String Line;
+            while((Line = reader.readLine()) != null){
+                String[] parts =  Line.split(",");
+                User user = new User(parts[0],parts[1]);
+                users.add(user);
+            }
+        } catch(IOException e){e.toString();}
+
+        return users;
+    }
+
+
+
+
+
+
+
 }
